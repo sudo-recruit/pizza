73a495ec2a596787c34af0d88f40a6939349fc07
ocowchun <ocowchun@gmail.com>
2015-01-08 16:17:21 +0800

    move attribute to attribute/default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,12 +11,6 @@ end
 rbenv_gem "bundler" do
 	ruby_version "2.2.0"
 end
-# node.default['nginx']['version']='1.6.0'
-node.default['nginx']['source']['version']='1.6.2'
-node.default['nginx']['source']['url']="http://nginx.org/download/nginx-1.6.2.tar.gz"
-node.default['nginx']['source']['checksum']="b5608c2959d3e7ad09b20fc8f9e5bd4bc87b3bc8ba5936a513c04ed8f1391a18"
-
-
 
 include_recipe "nginx::source"
 package 'nodejs'
@@ -45,17 +39,6 @@ template "/etc/security/limits.conf" do
 end
 
 # include_recipe "td-agent::default"
-
-
-# Array(node["rackbox"]["apps"]["unicorn"]).each_with_index do |app, index|
-#   default_port = node["rackbox"]["upstream_start_port"]["unicorn"].to_i + index
-#   app_dir      = ::File.join(node["appbox"]["apps_dir"], app["appname"], 'current')
-
-#   setup_nginx_site(app, app_dir, default_port)
-#   setup_unicorn_config(app, app_dir, default_port)
-#   setup_unicorn_runit(app, app_dir)
-# end
-
 # sudo apt-get install imagemagick
 
 # sudo apt-get install libmagickwand-dev
