782450e9c7b4b9a5717e1ca9c68b1783e7e8acbe
ocowchun <ocowchun@gmail.com>
2015-04-24 16:24:44 +0800

    add td-agent.conf and install redis

--- recipes/default.rb
+++ recipes/default.rb
@@ -22,6 +22,7 @@ package 'libmysqlclient-dev'
 package 'curl'
 package 'git'
 package 'vim'
+package 'redis-server'
 
 include_recipe "monit::default"
 
@@ -33,6 +34,14 @@ template "/etc/monit/conf.d/nginx.conf" do
   notifies :restart, "service[monit]",:delayed
 end
 
+template "/etc/monit/conf.d/redis.conf" do
+  owner "root"
+  group "root"
+  mode "0700"
+  source "monit_redis.conf.erb"
+  notifies :restart, "service[monit]",:delayed
+end
+
 template "/etc/security/limits.conf" do
   owner "root"
   group "root"
@@ -41,3 +50,10 @@ template "/etc/security/limits.conf" do
 end
 
 include_recipe "td-agent::default"
+
+template "/etc/td-agent/td-agent.conf" do
+  owner "root"
+  group "root"
+  mode "0704"
+  source "td-agent.conf.erb"
+end
