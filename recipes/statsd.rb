f697c4f0c129102fca19f9d244e2847480607a53
ocowchun <ocowchun@gmail.com>
2016-02-25 15:03:34 +0800

    add moro recipes

---
+++ recipes/statsd.rb
@@ -0,0 +1,23 @@
+require 'json'
+pita_ruby_version=node['pita']['ruby']['version']
+user_name=node['pita']['user_name']
+
+rbenv_gem "moro" do
+  ruby_version pita_ruby_version
+  version "0.0.8"
+end
+
+directory '/etc/moro' do
+end
+
+template "/etc/moro/config.json" do
+  mode "0777"
+  source "moro.config.json.erb"
+  variables :content=>JSON.generate(node['pita']['moro'])
+end
+
+poise_service 'moro' do
+  command "moro start --config /etc/moro/config.json"
+  user user_name
+  environment PATH:'/opt/rbenv/shims:/opt/rbenv/bin:/usr/local/bin:/usr/bin:/bin'
+end
