99f666b466ed77691171425a20df85e7b507a364
Heng-Yi Wu <henry40408@gmail.com>
2016-08-21 10:19:53 +0800

    install bower and webpack as global package as global packages

--- recipes/nodejs.rb
+++ recipes/nodejs.rb
@@ -1,3 +1,13 @@
 include_recipe "nodejs::default"
 include_recipe "nodejs::nodejs_from_binary"
 include_recipe "nodejs::npm"
+
+install_method = node["nodejs"]["install_method"]
+version = node["nodejs"]["version"]
+
+%w(bower webpack).each do |package|
+  nodejs_npm package
+  link "/usr/local/bin/#{package}" do
+    to "/usr/local/nodejs-#{install_method}-#{version}/bin/#{package}"
+  end
+end
