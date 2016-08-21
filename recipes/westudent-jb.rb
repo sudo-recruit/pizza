3bf2099b6b6f0e0a67c4121533a289c3db099ef8
Heng-Yi Wu <henry40408@gmail.com>
2016-08-21 12:03:27 +0800

    npm install, bower install and webpack in production mode

--- recipes/westudent-jb.rb
+++ recipes/westudent-jb.rb
@@ -14,3 +14,27 @@ execute "bundle_install" do
   group username
   user username
 end
+
+execute "bower_install" do
+  command "/usr/local/bin/bower install"
+  creates "#{deploy_to}/vendor/assets/components"
+  cwd deploy_to
+  group username
+  user username
+end
+
+execute "npm_install" do
+  command "/usr/local/bin/npm install"
+  creates "#{deploy_to}/node_modules"
+  cwd deploy_to
+  group username
+  user username
+end
+
+execute "webpack_production" do
+  command "/usr/local/bin/webpack -p"
+  creates "#{deploy_to}/app/assets/javascripts/bundle/app-bundle.js"
+  cwd deploy_to
+  group username
+  user username
+end
