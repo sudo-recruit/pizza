d8116855d67e1a8857b5371c238f45856920c4b4
ocowchun <ocowchun@gmail.com>
2016-10-18 17:42:11 +0800

    complete consul watch db:migrate

--- pizza/recipes/rails-tasks.rb
+++ pizza/recipes/rails-tasks.rb
@@ -1,17 +1,34 @@
 deploy_to = ckattr("pizza.deploy_to", node["pizza"]["deploy_to"], String)
 username = ckattr("pizza.username", node["pizza"]["username"], String)
 consul_user = 'consul'
+include_recipe "consul::default"
 
+# db migrate
 template "/home/#{username}/db_migrate.sh" do
   source 'rails/db_migrate.sh.erb'
+  group consul_user
+  mode "0755"
+  owner consul_user
+  variables deploy_to: deploy_to, username: username
+end
+
+consul_watch 'db-migrate' do
+  type 'event'
+  parameters(handler: "/home/#{username}/db_migrate.sh",name:'db-migrate')
+  notifies :reload, 'consul_service[consul]', :delayed
+end
+
+for test
+template "/home/#{username}/test.sh" do
+  source 'rails/test.sh.erb'
   group consul_user
   mode "0755"
   owner consul_user
   variables deploy_to: deploy_to
 end
 
-# consul_watch 'db-migrate' do
-#   type 'event'
-#   parameters(handler: "/home/#{username}/db_migrate.sh",name:'db-migrate')
-#   notifies :reload, 'consul_service[consul]', :delayed
-# end
+consul_watch 'foo-test' do
+  type 'event'
+  parameters(handler: "/home/#{username}/test.sh",name:'foo-test')
+  notifies :reload, 'consul_service[consul]', :delayed
+end
